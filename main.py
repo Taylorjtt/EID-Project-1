2c81ae017385ae3c7cf1dfecdd0dadbb1d38c6eb
unknown <taylorjtt@gmail.com>
2021-01-22 21:15:34 -0600

    adding master file

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 from TempSensor import TempSensor
+from master import Master
+from multiprocessing import Process
 
-t1 = TempSensor(1, 70);
+# https://stackoverflow.com/questions/18204782/runtimeerror-on-windows-trying-python-multiprocessing
+if __name__ == '__main__':
+    t1 = TempSensor(1, 70);
+    t2 = TempSensor(2, 70);
+    t3 = TempSensor(3, 70);
+    master = Master()
 
-t1.begin();
+    # https://stackoverflow.com/questions/26239695/parallel-execution-of-class-methods
+    process1 = Process(target=t1.begin)
+    process2 = Process(target=t2.begin)
+    process3 = Process(target=t3.begin)
+    masterProcess = Process(target=master.begin);
+
+    process1.start();
+    print("here")
+    process2.start();
+    process3.start();
+    masterProcess.start();
+
+    process1.join();
+    process2.join();
+    process3.join();
+    masterProcess.join();
+
+
---
+++ master.py
@@ -0,0 +1,86 @@
+# master system
+# John Taylor
+# UC Boulder
+# 01.21.2021
+
+import json
+import glob
+import time
+from tabulate import tabulate
+
+
+class Master:
+    sleepTime = 30  # seconds
+
+    def __init__(self):
+        self.errorCount = 0
+
+    def begin(self):
+        while True:
+            time.sleep(self.sleepTime)
+            print("Master Processing has begun")
+            # grab all the text files in the directory
+            textFiles = glob.glob('*.txt')
+            tableData = []
+            for fileName in textFiles:
+                try:
+                    with open(fileName) as f:
+                        data = json.load(f)
+                        # get the last 10 items in the list
+                        lastTenSamples = data["samples"][-10:]
+                        hi = self.getHighTemp(lastTenSamples)
+                        lo = self.getLowTemp(lastTenSamples)
+                        avg = self.getAvg(lastTenSamples)
+                        errorCount = lastTenSamples[-1]["error count"]
+                        alarmCount = lastTenSamples[-1]["alarm count"]
+                        sensorNumber = lastTenSamples[-1]["Sensor Number"]
+                        tableData.append(
+                            [str(sensorNumber), str(lo), str(avg), str(hi), str(errorCount), str(alarmCount)])
+                except OSError:
+                    print("Could not open/read file:", fileName)
+                    errorCount += 1;
+                    print("ErrorCount: " + errorCount)
+            print(tabulate(tableData, headers=["System", "Low", "Avg", "High", "Error Count", "Alarm Count"]))
+
+    def getHighTemp(self, data):
+        high = None
+        for sample in data:
+            try:
+                tempF = float(sample['temp'])
+                if high is None:
+                    high = tempF
+                elif tempF > float(high):
+                    high = tempF
+            except ValueError:
+                self.errorCount += 1
+        return str(high)
+
+    def getLowTemp(self, data):
+        low = None
+        for sample in data:
+            try:
+                tempF = float(sample['temp'])
+                if low is None:
+                    low = tempF
+                elif tempF < float(low):
+                    low = tempF
+            except ValueError:
+                self.errorCount += 1
+        return str(low)
+
+    def getAvg(self, data):
+        # sum of the average
+        total = 0
+        # number to average over
+        count = 0
+        for sample in data:
+            try:
+                tempF = float(sample['temp'])
+                total += tempF
+                count += 1
+            except ValueError:
+                self.errorCount += 1
+        if count > 0:
+            # prevent divide by 0 in the case of no data
+            average = total / count
+            return str(average)
